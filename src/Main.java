2c786ecc31bb77dbd2502986383819e12c8418d9
dariaost19 <dariaost19@gmail.com>
2023-03-27 23:39:53 -0400

    updates

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <10 ; i++) {
+        for (int i = 0; i <100 ; i++) {
             System.out.println(i);
 
+
         }
+        System.out.println("i am done");
     }
 }
